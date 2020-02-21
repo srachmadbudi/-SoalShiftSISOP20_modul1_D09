7d98ce6ea427a2dc10c75eff82ec603595ad352b
Rachmad Budi Santoso <57314114+srachmadbudi@users.noreply.github.com>
2020-02-21 18:05:04 +0700

    Update soal3.sh

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,1 +1,15 @@
+#number3a
+for i in 1..28
+do
+ wget -O pdkt_kusuma_$i loremflickr.com/320/240/cat -a wget.log
+done
 
+
+#number3b
+for i in 1..28
+do
+ wget -O pdkt_kusuma_$i loremflickr.com/320/240/cat -a wget.log
+done
+
+#number3c
+belum mas maaf
