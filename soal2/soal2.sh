749afbf119a4b60321eee410f7dbe4563a1a78dc
khofifahnurlaela <57974871+khofifahnurlaela@users.noreply.github.com>
2020-02-21 16:39:22 +0700

    Membuat password
    Password yang dihasilkan kemudian disimpan dalam sebuah file .txt sesuai dengan nama file yang diinputkan user.

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+
+var=$1
+
+if [[ $var =~ ^[A-Za-z.]+$ ]]
+        then cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 28 >> $var
+        else echo "Nama file hanya menggunakan alphabet."
+fi
+
