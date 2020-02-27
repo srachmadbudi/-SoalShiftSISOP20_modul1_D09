1c45d3f7341fbdd12bcd31fe066ad9d305f44c43
khofifahnurlaela <57974871+khofifahnurlaela@users.noreply.github.com>
2020-02-27 10:57:30 +0700

    Update nomer 3a

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -1,15 +1,5 @@
-#number3a
-for i in 1..28
+#number(3a)
+for i in {1..28}
 do
  wget -O pdkt_kusuma_$i loremflickr.com/320/240/cat -a wget.log
 done
-
-
-#number3b
-for i in 1..28
-do
- wget -O pdkt_kusuma_$i loremflickr.com/320/240/cat -a wget.log
-done
-
-#number3c
-belum mas maaf
