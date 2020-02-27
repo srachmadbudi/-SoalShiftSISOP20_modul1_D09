d7eb1ec195188040c30e46cb291fa4aebdf89801
khofifahnurlaela <57974871+khofifahnurlaela@users.noreply.github.com>
2020-02-27 10:52:18 +0700

    Update generate password

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -4,7 +4,11 @@
 var=$1
 
 if [[ $var =~ ^[A-Za-z.]+$ ]]
-        then cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 28 >> $var
+        then 
+        cat /dev/urandom | tr -dc 'a-z' | head -c 10 >> $var
+        cat /dev/urandom | tr -dc 'A-Z' | head -c 9 >> $var
+        cat /dev/urandom | tr -dc '0-9' | head -c 9 >> $var
+
         else echo "Nama file hanya menggunakan alphabet."
 fi
 
